3fdeb418b881f1c2417044a14584f4e6a3e3735f
ricardosantoscarvalho <46535773+ricardosantoscarvalho@users.noreply.github.com>
2020-01-02 17:22:10 +0000

    remove € and add currency from prestashop

--- para versao 1.6/multibanco/controllers/front/remember.php
+++ para versao 1.6/multibanco/controllers/front/remember.php
@@ -59,7 +59,7 @@ class MultibancoRememberModuleFrontController extends ModuleFrontController
 				'{lastname}' => $cliente->lastname,
 				'{entidade}' => $entidade,
 				'{referencia}' => chunk_split($referencia, 3, ' '),
-				'{total_paid}' => $valor . ' €',
+				'{total_paid}' => $valor . ' ' . Currency::getDefaultCurrency()->sign,
 				'{mb_logo}' => _PS_BASE_URL_ ._MODULE_DIR_.$this->module->name.'/multibanco.jpg'
 			);
 
